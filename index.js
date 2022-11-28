8f49781a5bc5a3147e0295f572e3e8d82fbc846f
RDA dev <dzingisob@gmail.com>
2022-11-28 13:57:31 +0000

    Update 5 files

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const app = express();
 const port = 4444;
 const path = require("path");
 
-app.use(express.static("public"));
+//app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", express.static("public"));
-app.use("/about", express.static("public"));
 app.use("/", express.static(path.join(__dirname, "public" + "index.html")));
+app.use("/about", express.static("public"));
 app.use(
   "/about",
   express.static(path.join(__dirname, "public" + "about.html"))
